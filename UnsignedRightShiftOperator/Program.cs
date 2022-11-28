2e4fbcb237cbc941265648e0af6ceaca56434c60
Kunlapath <kunlapath.paengsa@gmail.com>
2022-11-28 14:57:23 +0700

    Bitwise and shift operators

--- UnsignedRightShiftOperator/Program.cs
+++ UnsignedRightShiftOperator/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine($"After     >>: bin = {Convert.ToString(a, 2),32}, dec = {a}")
 int b = n >>> 2;
 Console.WriteLine($"After    >>>: bin = {Convert.ToString(b, 2),32}, dec = {b}");
 
-// Output:
+// Output: Bitwise and shift operators.
 // Before shift: bin = 11111111111111111111111111100000, dec = -32
 // After     >>: bin = 11111111111111111111111111111000, dec = -8
 // After    >>>: bin =   111111111111111111111111111000, dec = 1073741816
